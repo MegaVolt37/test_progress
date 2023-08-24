d19e3bbc6be3d59854697c47cebafed23b838891
Александр Смирнов <alexrus37reg@gmail.com>
2023-08-24 11:06:29 +0300

    Updates

---
+++ _nuxt/nuxt-icon.470df0da.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./nuxt-icon.vue.139b893a.js";import"./entry.6c4cee32.js";export{m as default};
--- _nuxt/nuxt-icon.6f51dd40.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./nuxt-icon.vue.5591e7b4.js";import"./entry.3f378674.js";export{m as default};
